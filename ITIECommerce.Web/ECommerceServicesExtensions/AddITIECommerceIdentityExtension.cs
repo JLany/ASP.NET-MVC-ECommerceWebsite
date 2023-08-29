229f3dc108e98d17ea75d1f8254037d6d3028955
Kilany <y.m.elkilany@gmail.com>
2023-08-29 21:34:10 +0300

    Fix project issue

---
+++ ITIECommerce.Web/ECommerceServicesExtensions/AddITIECommerceIdentityExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ITIECommerce.Web.ECommerceServicesExtensions
+{
+    public static class AddITIECommerceIdentityExtension
+    {
+        public static IdentityBuilder AddITIECommerceIdentity<TUser>(this IServiceCollection services
+            , Action<IdentityOptions> configureOptions) where TUser : IdentityUser
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = IdentityConstants.ApplicationScheme;
+                options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+            })
+                .AddIdentityCookies(options => { });
+
+            return services.AddIdentityCore<TUser>(options =>
+            {
+                options.Stores.MaxLengthForKeys = 128;
+                configureOptions?.Invoke(options);
+            })
+                .AddDefaultTokenProviders();
+        }
+    }
+}
